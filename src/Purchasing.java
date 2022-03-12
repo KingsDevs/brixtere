c9854333df51f29e3a50b71e02267537a670213c
KingsDevs <sunnyckair@gmail.com>
2022-03-12 20:29:27 +0800

    para modagan na ang purchasing sa jdoodle

--- src/Purchasing.java
+++ src/Purchasing.java
@@ -1,4 +1,4 @@
-import product.Product;
+import java.util.Scanner;
 
 public class Purchasing 
 {
@@ -10,5 +10,55 @@ public class Purchasing
         product.writeOutput();
 
         System.out.println("Amount due is " + product.getTotalCost());
+    }
+}
+
+class Product 
+{
+    private String itemName;
+    private double itemPrice;
+    private int itemQuantity;
+    private double amountDue;
+
+
+    public void setItemName(String newItemName)
+    {
+        itemName = newItemName;
+    }
+
+    public void setTotalCost(int quantity, double price)
+    {
+        amountDue = quantity * price;
+    }
+
+    public String getItemName()
+    {
+        return itemName;
+    }
+
+    public Double getTotalCost()
+    {
+        setTotalCost(itemQuantity, itemPrice);
+        return amountDue;
+    }
+
+    public void readInput()
+    {
+        try (Scanner s = new Scanner(System.in)) 
+        {
+            System.out.println("Enter the name of the item you are purchasing.");
+            itemName = s.nextLine();
+
+            System.out.println("Enter the quantity and price separated by a space");
+            itemQuantity = s.nextInt();
+            itemPrice = s.nextDouble();
+
+            
+        }
+    }
+
+    public void writeOutput()
+    {
+        System.out.println("You are purchasing " + itemQuantity + " " + itemName + "(s) at " + itemPrice + " each.");
     }
 }
